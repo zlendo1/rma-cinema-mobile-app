74dab050ba18cc2ea36358f0344bf5285e567d17
zlendo1 <zlendo1@etf.unsa.ba>
2023-04-02 21:49:46 +0200

    implemented HomeActivity

---
+++ app/src/main/java/ba/unsa/etf/gamespirala/GameStaticData.kt
@@ -0,0 +1,7 @@
+package ba.unsa.etf.gamespirala
+
+import ba.unsa.etf.gamespirala.staticdata.Game
+
+fun getGames(): List<Game> {
+    return listOf()
+}
--- app/src/main/java/ba/unsa/etf/gamespirala/HomeActivity.kt
+++ app/src/main/java/ba/unsa/etf/gamespirala/HomeActivity.kt
@@ -1,11 +1,52 @@
 package ba.unsa.etf.gamespirala
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.EditText
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import ba.unsa.etf.gamespirala.staticdata.Game
 
 class HomeActivity : AppCompatActivity() {
+
+    private lateinit var games: RecyclerView
+    private lateinit var gamesAdapter: GameListAdapter
+    private lateinit var searchText: EditText
+    private var gamesList: List<Game> = getGames()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        games = findViewById(R.id.game_list)
+        games.layoutManager = LinearLayoutManager(
+            this,
+            LinearLayoutManager.HORIZONTAL,
+            false
+        )
+
+        gamesAdapter = GameListAdapter(arrayListOf()) { game -> showGameDetails(game) }
+        games.adapter = gamesAdapter
+        gamesAdapter.updateGames(gamesList)
+
+        if (intent?.action == Intent.ACTION_SEND && intent?.type == "text/plain") {
+            handleSendText(intent)
+        }
     }
+
+    private fun showGameDetails(game: Game) {
+        val intent = Intent(this, GameDetailsActivity::class.java).apply {
+            putExtra("game_title", game.title)
+        }
+
+        startActivity(intent)
+    }
+
+    private fun handleSendText(intent: Intent) {
+        intent.getStringExtra(Intent.EXTRA_TEXT)?.let {
+            searchText.setText(it)
+        }
+    }
+
 }
