c282965bb8ed9c5f7b2e1b48bc5a3c2e379a34a3
zlendo1 <zlendo1@etf.unsa.ba>
2023-04-23 21:16:18 +0200

    bunch of renaming

---
+++ app/src/main/java/ba/unsa/etf/gamespirala/activity/MainActivity.kt
@@ -0,0 +1,15 @@
+package ba.unsa.etf.gamespirala.activity
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+
+}
