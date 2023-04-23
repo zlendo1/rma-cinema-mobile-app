1b00b4ea01c0b3080d87195bb6fb447a2064a7a0
zlendo1 <zlendo1@etf.unsa.ba>
2023-04-23 21:18:04 +0200

    bunch of renaming

--- app/src/main/java/ba/unsa/etf/gamespirala/activity/MainActivity.kt
+++ app/src/main/java/ba/unsa/etf/gamespirala/activity/MainActivity.kt
@@ -1,12 +1,6 @@
 package ba.unsa.etf.gamespirala.activity
 
 import android.os.Bundle
-<<<<<<< HEAD
-=======
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
->>>>>>> origin/master
 import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
