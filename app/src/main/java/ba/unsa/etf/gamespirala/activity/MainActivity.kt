9d8573d554cc2877c862aa26341c7ff6a9ab1855
zlendo1 <zlendo1@etf.unsa.ba>
2023-04-24 15:21:07 +0200

    moved starting activity to MainActivity

--- app/src/main/java/ba/unsa/etf/gamespirala/activity/MainActivity.kt
+++ app/src/main/java/ba/unsa/etf/gamespirala/activity/MainActivity.kt
@@ -1,12 +1,26 @@
 package ba.unsa.etf.gamespirala.activity
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.EditText
 import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupWithNavController
+import ba.unsa.etf.gamespirala.R
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        setContentView(R.layout.activity_main)
+
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+        val navController = navHostFragment.navController
+
+        val navView: BottomNavigationView = findViewById(R.id.bottom_nav)
+        navView.setupWithNavController(navController)
     }
 
 }
