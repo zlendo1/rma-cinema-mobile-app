83d0eaad2b426d1706be352409e69d7c612103c7
zlendo1 <zlendo1@etf.unsa.ba>
2023-04-26 20:38:11 +0200

    code beautification

--- app/src/main/java/ba/unsa/etf/gamespirala/activity/OrientationChange.kt
+++ app/src/main/java/ba/unsa/etf/gamespirala/activity/OrientationChange.kt
@@ -5,10 +5,11 @@ import android.content.res.Configuration
 object OrientationChange {
 
     fun onOrientation(configuration: Configuration, onPortrait: () -> Unit, onLandscape: () -> Unit) {
-        if (configuration.orientation == Configuration.ORIENTATION_PORTRAIT) {
-            onPortrait()
-        } else {
-            onLandscape()
+        when (val orientation = configuration.orientation) {
+            Configuration.ORIENTATION_PORTRAIT -> onPortrait()
+            Configuration.ORIENTATION_LANDSCAPE -> onLandscape()
+
+            else -> throw IllegalStateException("Orientation configuration not allowed for orientation with ID: $orientation")
         }
     }
 
