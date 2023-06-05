3fa41a6446bc2354bf195a8c36bad16b1885c1f8
zlendo1 <zlendo1@etf.unsa.ba>
2023-06-06 01:03:00 +0200

    auxiliary functions added

---
+++ app/src/main/java/ba/etf/rma23/projekat/auxiliary/Auxiliary.kt
@@ -0,0 +1,40 @@
+package ba.etf.rma23.projekat.auxiliary
+
+fun ratingToEsrb(rating: Int): String {
+    return when (rating) {
+        1 -> "Three"
+        2 -> "Seven"
+        3 -> "Twelve"
+        4 -> "Sixteen"
+        5 -> "Eighteen"
+        7 -> "EC"
+        8 -> "E"
+        9 -> "E10"
+        10 -> "T"
+        11 -> "M"
+        12 -> "AO"
+        else -> ""
+    }
+}
+
+fun esrbToAge(esrbRating: String): Int {
+    return when (esrbRating) {
+        "Three" -> 3
+        "Seven" -> 7
+        "Twelve" -> 12
+        "Sixteen" -> 16
+        "Eighteen" -> 18
+        "EC" -> 3
+        "E" -> 6
+        "E10" -> 10
+        "T" -> 13
+        "M" -> 17
+        "AO" -> 18
+        else -> 0
+    }
+}
+
+fun timestampToString(timestamp: Int): String {
+    return java.time.format.DateTimeFormatter.ISO_INSTANT
+        .format(java.time.Instant.ofEpochSecond(timestamp.toLong()))
+}
