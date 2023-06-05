c748ca3fc87a9fc3d8acd71d00456f101d62acfd
zlendo1 <zlendo1@etf.unsa.ba>
2023-06-06 01:05:04 +0200

    added classes for working with AccountApi

---
+++ app/src/main/java/ba/etf/rma23/projekat/data/repositories/result/GameResultAccount.kt
@@ -0,0 +1,6 @@
+package ba.etf.rma23.projekat.data.repositories.result
+
+data class GameResultAccount(
+    val igdb_id: Int,
+    val name: String
+)
---
+++ app/src/main/java/ba/etf/rma23/projekat/domain/Account.kt
@@ -0,0 +1,7 @@
+package ba.etf.rma23.projekat.domain
+
+data class Account(
+    var acHash: String,
+    var student: String,
+    var age: Int
+)
